fc5826e5a9b001b778cb1d46bfc5f956def08b49
unknown <naila.ahmedli1995@gmail.com>
2022-07-25 15:27:33 +0400

    clientsSay section is done

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+
+
+
+    $('.clientsSlider').slick({
+      centerMode: true,
+      dots: true,
+      initialSlide: 1,
+      slidesToShow: 2,
+      arrows: false,
+      centerPadding: '200px',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+            centerPadding: '50px',
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerPadding: '50px',
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerPadding: '30px',
+          }
+        }
+      ]
+    });
+
+});
