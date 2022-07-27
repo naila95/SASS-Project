86dbb398a80b14f67a2da622531c859bd19b70fe
unknown <naila.ahmedli1995@gmail.com>
2022-07-27 12:16:26 +0400

    footer is done

--- js/main.js
+++ js/main.js
@@ -46,6 +46,35 @@ $(document).ready(function(){
         dots: true,
         initialSlide: 1,
         slidesToShow: 2,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                centerPadding: '50px',
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerPadding: '50px',
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerPadding: '30px',
+              }
+            }
+          ]
       });
 
 });
